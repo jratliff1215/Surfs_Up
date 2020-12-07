e2665325ae74ed58a291cb2a571d56f5c7659c76
jratliff1215 <72112461+jratliff1215@users.noreply.github.com>
2020-12-06 19:40:06 -0600

    Add files via upload

---
+++ WeatherData (Module Nine)/Module 9 SQLite and Flask.py
@@ -0,0 +1,12 @@
+Module 9 SQLite and Flask
+
+from Flask import Flask
+app = Flash(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+export FLASK_APP=app.py
+
+flask return
